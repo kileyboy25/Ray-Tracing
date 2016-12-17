12b1cb85e25fa4232a6bb4da59cf3fa78f5ea12b
Mayur Argekar <mayur.arge@gmail.com>
2016-12-17 00:50:22 -0600

    Revert back to the working set from RT-1.

--- RayTracer/viewing_plane.h
+++ RayTracer/viewing_plane.h
@@ -1,5 +1,5 @@
-#ifndef VIEWINGPLANE_H
-#define VIEWINGPLANE_H
+#ifndef VIEWING_PLANE_H
+#define VIEWING_PLANE_H
 #include "vector.h"
 #include "camera.h"
 class ViewingPlane{
@@ -12,3 +12,22 @@ private:
 	Vec planeStart, planeCenter;
 };
 #endif
+ViewingPlane::ViewingPlane(Camera camera, double d, double worldWidth, double worldHeight){
+	planeCenter = Vec(camera.getPosition().getX() - camera.getCameraN().getX()*d,
+		camera.getPosition().getY() - camera.getCameraN().getY()*d,
+		camera.getPosition().getZ() - camera.getCameraN().getZ()*d);
+	planeStart = Vec(planeCenter.getX() - camera.getCameraU().getX()*worldWidth / 2 - camera.getCameraV().getX()*worldHeight / 2,
+		planeCenter.getY() - camera.getCameraU().getY()*worldWidth / 2 - camera.getCameraV().getY()*worldHeight / 2,
+		planeCenter.getZ() - camera.getCameraU().getZ()*worldWidth / 2 - camera.getCameraV().getZ()*worldHeight / 2);
+}
+
+Vec ViewingPlane::getPlaneStart(){
+	return planeStart;
+}
+
+Vec ViewingPlane::getPlanePoint(Camera camera, int i, int j, double dx, double dy){
+	Vec temp(this->getPlaneStart().getX() + camera.getCameraU().getX()*(i + 0.5)*dx + camera.getCameraV().getX()*(j + 0.5)*dy,
+		this->getPlaneStart().getY() + camera.getCameraU().getY()*(i + 0.5)*dx + camera.getCameraV().getY()*(j + 0.5)*dy,
+		this->getPlaneStart().getZ() + camera.getCameraU().getZ()*(i + 0.5)*dx + camera.getCameraV().getZ()*(j + 0.5)*dy);
+	return temp;
+}
